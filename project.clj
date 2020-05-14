fd77ecf84fcf884fd13b68772f86247f3dee5b63
Pierre-Yves Ritschard <pyr@spootnik.org>
2020-05-14 10:05:19 +0200

    Version 0.1.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject exoscale/lein-replace "0.1.0-SNAPSHOT"
+(defproject exoscale/lein-replace "0.1.0"
   :description "Transform your leiningen project with replace expressions"
   :url "https://github.com/exoscale/lein-replace"
   :deploy-repositories [["releases" :clojars] ["snapshots" :clojars]]
